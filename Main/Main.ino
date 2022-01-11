71ecb92e3be121a594f28e894918f0bf9791d9b5
Arky <arky@arkyslab.com>
2022-01-11 02:47:55 -0500

    Initial work on animation

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,13 +4,32 @@
 // Assets
 #include "lcars.h"
 
+//TFT_eSPI instance
+TFT_eSPI tft = TFT_eSPI();
+
 // Constants
 const int DISPLAY_WIDTH = 320;
 const int DISPLAY_HEIGHT = 240;
 
-TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+// Sprite Setup
+
+TFT_eSprite sprite = TFT_eSprite(&tft);
+TFT_eSprite background = TFT_eSprite(&tft);
+
+// Variables
+int minY = 50;
+int maxY = 180;
+int spriteY = minY;
+int oldY = minY;
+
+bool movingDown = true;
+
+
+
 
 void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
   
   // Initialize TFT
   TFTSetup();
@@ -19,9 +38,34 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   
   DrawBackgroundImage(lcars);
+
+  
 }
 
 void loop() { 
+
+  background.createSprite(5, 10);
+  background.fillSprite(TFT_BLACK);
+  background.pushSprite(80, oldY);
+  background.deleteSprite();
+  
+  sprite.createSprite(5, 10);
+  sprite.fillSprite(TFT_RED);
+  sprite.pushSprite(80, spriteY);
+  sprite.deleteSprite();
+  oldY = spriteY;
+  delay(10);
+  if (movingDown){ 
+   spriteY++;  
+  }
+  else {
+   spriteY--;
+   
+  }
+
+  if (spriteY <= minY) { movingDown = true; }
+  if (spriteY >= maxY) { movingDown = false; }
+  
 
 }
 
@@ -44,4 +88,11 @@ void DrawBackgroundImage(const unsigned short IMAGE[]) {
   // Draw the background image
   tft.pushImage(0,0, DISPLAY_WIDTH, DISPLAY_HEIGHT, IMAGE);
       
+}
+
+void BlinkLED() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
 }
